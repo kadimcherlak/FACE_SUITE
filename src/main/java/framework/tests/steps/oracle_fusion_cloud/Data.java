56e7e8c96977bc12b3f8a0468ac15fbe9bc5cf08
4rven <4rven@DESKTOP-J75I4T5>
2019-02-13 00:28:29 -0500

    Updated till Change Manager submission scenario

--- src/main/java/framework/tests/steps/oracle_fusion_cloud/Data.java
+++ src/main/java/framework/tests/steps/oracle_fusion_cloud/Data.java
@@ -1,62 +1,61 @@
 package framework.tests.steps.oracle_fusion_cloud;
+
 import java.util.Random;
 
 public class Data extends framework.core.models.Data {
 
-    // Alpha sorted list of models items (yes it will be long normally)
-    private String addressLine1;
-    private String addressLine2;
-    private String assignmentCategory;
-    private String businessUnit;
-    private String city;
-    private String companyEntity;
-    private String costCenter;
-    private String country;
-    private String countryCode;
-    private String county;
-    private String dateOfBirth;
-    private String department;
-    private String email;
-    private String erProfitCenter;
-    private String firstName;
-    private String gender;
-    private String grade;
-    private String hireAction;
-    private String hireDate;
-    private String hireReason;
-    private String hourlyPaidOrSalaried;
-    private String job;
-    private String lastName;
-    private String legalEmployer;
-    private String location;
-    private String maritalStatus;
-    private String nationalID;
-    private String nationalIDType;
-    private String newlySeparatedVeteranDischargeDate;
-    private String number;
-    private String password;
-    private String payroll;
-    private String pearsonEmailRequired;
-    private String salaryAmount;
-    private String salaryBasis;
-    private String state;
-    private String type1;
-    private String type2;
-    private String userName;
-    private String veteranSelfIdentificationStatus;
-    private String workerType;
-    private String workingAsManager;
-    private String zipCode;
-    private String personNumber;
-    private String elementName;
-    private String effectiveAsOfDate;
-    private String employeeManagerChange;
-    private String changeManagerReason;
-    private String managerName;
+	// Alpha sorted list of models items (yes it will be long normally)
+	private String addressLine1;
+	private String addressLine2;
+	private String assignmentCategory;
+	private String businessUnit;
+	private String city;
+	private String companyEntity;
+	private String costCenter;
+	private String country;
+	private String countryCode;
+	private String county;
+	private String dateOfBirth;
+	private String department;
+	private String email;
+	private String erProfitCenter;
+	private String firstName;
+	private String gender;
+	private String grade;
+	private String hireAction;
+	private String hireDate;
+	private String hireReason;
+	private String hourlyPaidOrSalaried;
+	private String job;
+	private String lastName;
+	private String legalEmployer;
+	private String location;
+	private String maritalStatus;
+	private String nationalID;
+	private String nationalIDType;
+	private String newlySeparatedVeteranDischargeDate;
+	private String number;
+	private String password;
+	private String payroll;
+	private String pearsonEmailRequired;
+	private String salaryAmount;
+	private String salaryBasis;
+	private String state;
+	private String type1;
+	private String type2;
+	private String userName;
+	private String veteranSelfIdentificationStatus;
+	private String workerType;
+	private String workingAsManager;
+	private String zipCode;
+	private String personNumber;
+	private String elementName;
+	private String effectiveAsOfDate;
+	private String employeeManagerChange;
+	private String changeManagerReason;
+	private String managerName;
 
-   
-     
-    public String getPersonNumber() {
+	public String getPersonNumber() {
 		return personNumber;
 	}
 
@@ -65,229 +64,222 @@ public class Data extends framework.core.models.Data {
 	}
 
 	// Alpha sorted list of getters
-    // most of these items get loaded via bean loader, so normally setters not needed.
-    public String getAddressLine1() {
-        return addressLine1;
-    }
+	// most of these items get loaded via bean loader, so normally setters not
+	// needed.
+	public String getAddressLine1() {
+		return addressLine1;
+	}
 
-    public String getAddressLine2() {
-        return addressLine2;
-    }
+	public String getAddressLine2() {
+		return addressLine2;
+	}
 
-    public String getAssignmentCategory() {
-        return assignmentCategory;
-    }
+	public String getAssignmentCategory() {
+		return assignmentCategory;
+	}
 
-    public String getBusinessUnit() {
-        return businessUnit;
-    }
+	public String getBusinessUnit() {
+		return businessUnit;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public String getCompanyEntity() {
-        return companyEntity;
-    }
+	public String getCompanyEntity() {
+		return companyEntity;
+	}
 
-    public String getCostCenter() {
-        return costCenter;
-    }
+	public String getCostCenter() {
+		return costCenter;
+	}
 
-    public String getCountry() {
-        return country;
-    }
+	public String getCountry() {
+		return country;
+	}
 
-    public String getCountryCode() {
-        return countryCode;
-    }
+	public String getCountryCode() {
+		return countryCode;
+	}
 
-    public String getCounty() {
-        return county;
-    }
+	public String getCounty() {
+		return county;
+	}
 
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-    public String getDepartment() {
-        return department;
-    }
-    
-    public String geteffectiveAsOfDate() {
-        return effectiveAsOfDate;
-    }
+	public String getDepartment() {
+		return department;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String geteffectiveAsOfDate() {
+		return effectiveAsOfDate;
+	}
 
-    public String getErProfitCenter() {
-        return erProfitCenter;
-    }
-    
-    public String getelementName() {
-        return elementName;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getFirstName() {
-    	int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 8;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int) 
-              (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String firstName = "FAUTO"+buffer.toString().toUpperCase();
-        return firstName;
-        
-    }
+	public String getErProfitCenter() {
+		return erProfitCenter;
+	}
 
-    public String getGender() {
-        return gender;
-    }
+	public String getelementName() {
+		return elementName;
+	}
 
-    public String getGrade() {
-        return grade;
-    }
+	public String getFirstName() {
+		int leftLimit = 97; // letter 'a'
+		int rightLimit = 122; // letter 'z'
+		int targetStringLength = 8;
+		Random random = new Random();
+		StringBuilder buffer = new StringBuilder(targetStringLength);
+		for (int i = 0; i < targetStringLength; i++) {
+			int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+			buffer.append((char) randomLimitedInt);
+		}
+		String firstName = "FAUTO" + buffer.toString().toUpperCase();
+		return firstName;
 
-    public String getHireAction() {
-        return hireAction;
-    }
+	}
 
-    public String getHireDate() {
-        return hireDate;
-    }
+	public String getGender() {
+		return gender;
+	}
 
-    public String getHireReason() {
-        return hireReason;
-    }
+	public String getGrade() {
+		return grade;
+	}
 
-    public String getHourlyPaidOrSalaried() {
-        return hourlyPaidOrSalaried;
-    }
+	public String getHireAction() {
+		return hireAction;
+	}
 
-    public String getJob() {
-        return job;
-    }
+	public String getHireDate() {
+		return hireDate;
+	}
 
-    public String getLastName() {
-    	int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 8;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int) 
-              (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String lastName = "LAUTO"+buffer.toString().toUpperCase();
-        return lastName;
-    }
+	public String getHireReason() {
+		return hireReason;
+	}
 
+	public String getHourlyPaidOrSalaried() {
+		return hourlyPaidOrSalaried;
+	}
 
-    public String getLegalEmployer() {
-        return legalEmployer;
-    }
+	public String getJob() {
+		return job;
+	}
 
-    public String getLocation() {
-        return location;
-    }
+	public String getLastName() {
+		int leftLimit = 97; // letter 'a'
+		int rightLimit = 122; // letter 'z'
+		int targetStringLength = 8;
+		Random random = new Random();
+		StringBuilder buffer = new StringBuilder(targetStringLength);
+		for (int i = 0; i < targetStringLength; i++) {
+			int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+			buffer.append((char) randomLimitedInt);
+		}
+		String lastName = "LAUTO" + buffer.toString().toUpperCase();
+		return lastName;
+	}
 
-    public String getMaritalStatus() {
-        return maritalStatus;
-    }
+	public String getLegalEmployer() {
+		return legalEmployer;
+	}
 
-    public String getNationalID() {
-    	Random rnd = new Random();
-    	String ssn_temp= String.format("%09d", rnd.nextInt(1000000000));
-    	 String nationalID = String.valueOf(ssn_temp).replaceFirst("(\\d{3})(\\d{2})(\\d+)", "$1-$2-$3");
-        return nationalID;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public String getNationalIDType() {
-        return nationalIDType;
-    }
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
 
-    public String getNumber() {
-        return number;
-    }
+	public String getNationalID() {
+		Random rnd = new Random();
+		String ssn_temp = String.format("%09d", rnd.nextInt(1000000000));
+		String nationalID = String.valueOf(ssn_temp).replaceFirst("(\\d{3})(\\d{2})(\\d+)", "$1-$2-$3");
+		return nationalID;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getNationalIDType() {
+		return nationalIDType;
+	}
 
-    public String getPayroll() {
-        return payroll;
-    }
+	public String getNumber() {
+		return number;
+	}
 
-    public String getPearsonEmailRequired() {
-        return pearsonEmailRequired;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getSalaryAmount() {
-        return salaryAmount;
-    }
+	public String getPayroll() {
+		return payroll;
+	}
 
-    public String getSalaryBasis() {
-        return salaryBasis;
-    }
+	public String getPearsonEmailRequired() {
+		return pearsonEmailRequired;
+	}
 
-    public String getState() {
-        return state;
-    }
+	public String getSalaryAmount() {
+		return salaryAmount;
+	}
 
-    public String getType1() {
-        return type1;
-    }
+	public String getSalaryBasis() {
+		return salaryBasis;
+	}
 
-    public String getType2() {
-        return type2;
-    }
+	public String getState() {
+		return state;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getType1() {
+		return type1;
+	}
 
-    public String getVeteranSelfIdentificationStatus() {
-        return veteranSelfIdentificationStatus;
-    }
-    
-    public String getnewlySeparatedVeteranDischargeDate() {
-        return newlySeparatedVeteranDischargeDate;
-    }
-    
-    
+	public String getType2() {
+		return type2;
+	}
 
-    public String getWorkerType() {
-        return workerType;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public String getWorkingAsManager() {
-        return workingAsManager;
-    }
+	public String getVeteranSelfIdentificationStatus() {
+		return veteranSelfIdentificationStatus;
+	}
 
-    public String getZipCode() {
-        return zipCode;
-    }
-    
-    public String employeeManagerChange() {
-        return employeeManagerChange;
-    }
-    
-    public String changeManagerReason() {
-        return changeManagerReason;
-    }
-    
-    public String managerName() {
-        return managerName;
-    }
+	public String getnewlySeparatedVeteranDischargeDate() {
+		return newlySeparatedVeteranDischargeDate;
+	}
 
-	
+	public String getWorkerType() {
+		return workerType;
+	}
+
+	public String getWorkingAsManager() {
+		return workingAsManager;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public String getEmployeeManagerChange() {
+		return employeeManagerChange;
+	}
+
+	public String getChangeManagerReason() {
+		return changeManagerReason;
+	}
+
+	public String getManagerName() {
+		return managerName;
+	}
+
 }
-
-
--- src/main/resources/features/oracle_fusion_cloud/ChangeManagerTransactionApproval.feature
+++ src/main/resources/features/oracle_fusion_cloud/ChangeManagerTransactionApproval.feature
@@ -9,13 +9,14 @@ Scenario Outline: As an Line Manager, i should be able to Perform Change Manager
     When user clicks on Navigator icon
     And user clicks on My Team link under My Team section
     Then My Team page should be displayed
-    And user clicks on the Actions icon of the Employee 
-    When user navigates to Personal and Employement
-    And user clicks on Change Manager option
-     And user enter details in Change Manager tab
-     And user clicks on Review button
+    And user clicks on the Employee link
+    When user navigates to Change Manager option
+     Then Change Manager page should be displayed
+     And user Select value for Change Manager Reason from dropdown
+     And user Select the value in Manager dropdown 
+     And user click on Review button in Change Manager page 
     Then the current value and proposed values are displayed correctly in Review Page
-    When user clicks on Submit button
+    When user clicks on Submit button in Change Manager page
     Then The transaction should be saved properly and My Team page should be displayed
      And user click on Sign Out option
     Then Sign out should be successful
@@ -25,20 +26,20 @@ Scenario Outline: As an Line Manager, i should be able to Perform Change Manager
       | Change Manager |
       
  
-  @Line_manager1_approves_change_manager_request
-Scenario Outline: As an Line Manager 1, i should be able to approves request of employee's manager change
-    Given user runs <Test Scenario>
-    And user login to Oracle Applications Cloud web page
-    When user clicks on Notifications bell icon
-    And user clicks on Action Required Notification link
-    Then notification details should be opened in new window with employee's manager change details
-    When user clicks on Approve button
-    Then The request should be approved and home page should be displayed
-    And user click on Sign Out option
-    Then Sign out should be successful
-    
-    Examples:
-      | Test Scenario  |
-      | Change Manager |
-      
+  #@Line_manager1_approves_change_manager_request
+#Scenario Outline: As an Line Manager 1, i should be able to approves request of employee's manager change
+    #Given user runs <Test Scenario>
+    #And user login to Oracle Applications Cloud web page
+    #When user clicks on Notifications bell icon
+    #And user clicks on Action Required Notification link
+    #Then notification details should be opened in new window with employee's manager change details
+    #When user clicks on Approve button
+    #Then The request should be approved and home page should be displayed
+    #And user click on Sign Out option
+    #Then Sign out should be successful
+    #
+    #Examples:
+      #| Test Scenario  |
+      #| Change Manager |
+      #
  
